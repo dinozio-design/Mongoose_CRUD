d9ef3db62df67e2f47419fe4c55582e77ef76ae4
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2024-01-17 15:43:55 -0500

    reworked the connection module with newURLParser and UnifiedTopology

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 
 // local connection to MongoDB wraped in mongoose
-mongoose.connect("mongodb://127.0.0.1:27017/mongooseCRUDPracticesDB");
+mongoose.connect("mongodb://127.0.0.1:27017/mongooseCRUDPracticesDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 // exporting connection
 export default mongoose.connection;
