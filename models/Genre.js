b14b6fa05e06e27588b5187f8181ede7759cc366
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2024-01-22 13:36:07 -0500

    more comments on Genre

--- models/Genre.js
+++ models/Genre.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
+
+// Defining a Mongoose schema for the 'Genre' model
 const genreSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -11,13 +13,17 @@ const genreSchema = new mongoose.Schema({
     }
 });
 
+// Creating a mongoose model named "Genre" based on schema defined in "genreSchem"
 const Genre = mongoose.model("Genre", genreSchema);
+
+
+// Defining a function named 'handleError' that takes an error ('err') as a parameter
+// Loging the error to the console using console.error
 const handleError = (err)=> console.error(err);
 
 
-// Following code will add data only if the collection is empty in order to prevent any duplicates
+// The following code will add data only if the collection is empty in order to prevent any duplicates
 // note that more than one document may have the same value
-
 Genre.find({})
     .exec()
     .then(collection =>{
